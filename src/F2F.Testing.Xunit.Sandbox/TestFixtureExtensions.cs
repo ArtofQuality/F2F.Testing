a4c617427606cb024055c23ad279e0a662a50870
Florian Schinagl <fs@think-clean.de>
2015-04-13 10:30:12 +0200

    Add extension methods for using fakeiteasy, sandbox in xUnit #19

---
+++ src/F2F.Testing.Xunit.Sandbox/TestFixtureExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using F2F.Testing.Sandbox;
+
+namespace F2F.Testing.Xunit.Sandbox
+{
+	/// <summary>
+	/// Extension methods for TestFixture.
+	/// </summary>
+	public static class TestFixtureExtensions
+	{
+		/// <summary>
+		/// Wrapper for Use&lt;FileSandboxFeature&gt;().Fixture.
+		/// </summary>
+		/// <param name="fixture">The test fixture</param>
+		/// <returns>The file sandbox instance.</returns>
+		public static IFileSandbox Sandbox(this TestFixture fixture)
+		{
+			return fixture.Use<FileSandboxFeature>().Sandbox;
+		}
+	}
+}
